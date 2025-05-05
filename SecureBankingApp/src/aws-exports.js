f7ba774e5dc0bad84e297eb4fab0ea118164396b
ThomasCGean <tgean1@toromail.csudh.edu>
2025-05-05 12:15:45 -0700

    In Progress

--- SecureBankingApp/src/aws-exports.js
+++ SecureBankingApp/src/aws-exports.js
@@ -3,7 +3,7 @@ const awsExports = {
     region: 'us-east-1',
     userPoolId: 'us-east-1_qAYkxZlSp',
     userPoolWebClientId: '15ocuqh4vnvq7bo26pobp7iesl',
-    mandatorySignIn: true, 
+    mandatorySignIn: true,
   }
 };
 
