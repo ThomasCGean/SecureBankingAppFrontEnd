9024249a68d2e41afce215bd82d94a5aa2fd178c
ThomasCGean <tgean1@toromail.csudh.edu>
2025-05-05 12:42:45 -0700

    Added AWS Cognito integration

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,9 +1,15 @@
 const awsExports = {
-  Auth: {
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_qAYkxZlSp',
-    userPoolWebClientId: '15ocuqh4vnvq7bo26pobp7iesl',
-    mandatorySignIn: true,
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_qAYkxZlSp',
+  aws_user_pools_web_client_id: '15ocuqh4vnvq7bo26pobp7iesl',
+  aws_mandatory_sign_in: 'enable',
+  oauth: {
+    domain: 'your-domain.auth.us-east-1.amazoncognito.com',
+    scope: ['email', 'openid', 'profile'],
+    redirectSignIn: 'http://localhost:5173/',
+    redirectSignOut: 'http://localhost:5173/',
+    responseType: 'code'
   }
 };
 
